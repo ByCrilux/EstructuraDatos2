072ca8f737b1d11bd99fde311142f4d7beca7914
ByCrilux <cristhiants20@gmail.com>
2024-08-24 20:20:27 -0400

    proyecto agregado

---
+++ ClaseCalculadoraSimple.py
@@ -0,0 +1,22 @@
+class Calculator():
+    """"Una simple calculadora que realiza operaciones básicas"""
+    def __init__(self):
+        self._a = 0
+        self._b = 0
+        
+    def pedir_valores(self, valor_1, valor_2):
+        """Asigna los valores a y b para las operaciones"""
+        self._a = valor_1
+        self._b = valor_2
+
+    def sumar(self):
+        return self._a + self._b
+    
+    def restar(self):
+        return self._a - self._b
+    
+    def multiplicar(self):
+        return self._a * self._b
+    
+    def devidir(self):
+        return self._a / self._b
---
+++ test.py
@@ -0,0 +1,19 @@
+from ClaseCalculadoraSimple import Calculator
+arit = Calculator()
+print(f"1 sumar\n2 restar\n3 multiplicar\n4 dividir\n5 salir")
+valor_1 = float(input("introducir valor 1: "))
+valor_2 = float(input("introducir valor 2: "))
+arit.pedir_valores(valor_1, valor_2)
+opcion = 0
+
+while opcion != "5":
+    opcion = input("elija una operacion: ")
+    match opcion:
+        case "1":
+            print(f"la suma es: {arit.sumar()}")
+        case "2":
+            print(f"la resta es: {arit.restar()}")
+        case "3":
+            print(f"la multiplicacion es: {arit.multiplicar()}")
+        case "4":
+            print(f"la division es: {arit.devidir()}")
